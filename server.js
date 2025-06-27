4527b047386fc8fd00936301a8cf84acb7e00486
krishna123-png <khirani787@gmail.com>
2025-06-27 13:22:55 +0530

    Updated allow origin header in cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: 'https://internal-tools-frontend.vercel.app/',
+        origin: 'https://internal-tools-frontend.vercel.app',
         credentials: true
     }
 ));
